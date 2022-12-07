3828f9cf2ee2f83173251d1db8888a91d43b6aef
Ryan Belcher <ryanbelcher@ryans-air.myfiosgateway.com>
2022-12-06 19:30:59 -0500

    started engineer and intern functions

--- index.js
+++ index.js
@@ -1,38 +1,89 @@
+const manager = 
+const engineer =
+const intern = 
 const inquirer = require('inquirer');
 
-const createManager = [
-    {
-        type: 'input',
-        message: `What is your team Manager's name?`,
-        name: 'managerName',
-        validate: managerInput => {
-            if (managerInput) {
-                return true;
-            } else {
-                console.log(`You must enter your team manager's name!`);
+function createManager() {
+    inquirer.prompt(
+        [
+            {
+                type: 'input',
+                message: `What is your team Manager's name?`,
+                name: 'managerName',
+                validate: managerInput => {
+                    if (managerInput) {
+                        return true;
+                    } else {
+                        console.log(`You must enter your team manager's name!`);
+                    }
+                }
+            },
+            {
+                type: 'number',
+                message: `What is your team manager's ID number?`,
+                name: 'managerId'
+            },
+            {
+                type: 'input',
+                message: `What is your team manager's email address?`,
+                name: 'managerEmail'
+            },
+            {
+                type: 'number',
+                message: `What is your team manager's office number?`,
+                name: 'office'
             }
-        }
-    },
-    {
-        type: 'number',
-        message: `What is your team manager's ID number?`,
-        name: 'managerId'
-    },
-    {
-        type: 'input',
-        message: `What is your team manager's email address?`,
-        name: 'managerEmail'
-    },
-    {
-        type: 'number',
-        message: `What is your team manager's office number?`,
-        name: 'office'
-    }
-]
-
-function init() {
-    inquirer
-        .prompt(createManager)
+        ])
 }
 
-init();
+
+function createEngineer() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: `What is your engineer's name?`,
+            name: 'engineerName'
+        },
+        {
+            type: 'number',
+            message: `What is your engineer's ID number?`,
+            name: 'engineerId'
+        },
+        {
+            type: 'input',
+            message: `What is your engineer's email address?`,
+            name: 'engineerEmail'
+        },
+        {
+            type: 'input',
+            message: `What is your engineer's GitHub?`,
+            name: 'engineerGithub'
+        }
+    ])
+}
+
+
+function createIntern() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: `What is your intern's name?`,
+            name: 'internName'
+        },
+        {
+            type: 'number',
+            message: `What is your intern's ID number?`,
+            name: 'internId'
+        },
+        {
+            type: 'input',
+            message: `What is your intern's email address?`,
+            name: 'interEmail'
+        },
+        {
+            type: 'input',
+            message: `What is your intern's school?`,
+            name: 'internSchool'
+        }
+    ])
+} 
