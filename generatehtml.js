dffd7bdbd5114a0bb6a525f465f52e33b3866953
Ryan Belcher <ryanbelcher@ryans-air.myfiosgateway.com>
2022-12-07 21:23:28 -0500

    added boilerplate html

--- generatehtml.js
+++ generatehtml.js
@@ -3,7 +3,31 @@ const { mainModule } = require('process');
 function generateHTML(manager, engineer, intern) {
     console.log('data in generate html', data);
     return `
-  
+    <!DOCTYPE html>
+    <html lang='en'>
+    
+    <head>
+        <meta charset='UTF-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'
+            integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
+        <link rel='stylesheet' href='sandbox.css'>
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css'
+            integrity='sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=='
+            crossorigin='anonymous' referrerpolicy='no-referrer' />
+        <title>Team Awesome</title>
+    </head>
+    <body>
+    <nav id='custom-nav' class='navbar navbar-light d-flex justify-content-center'>
+        <h1>Team Awesome</h1>
+    </nav>
+    <div id='custom-div' class='row row-cols-1 row-cols-md-3 g-4 d-flex justify-content-center'>
+    
+    </div>
+    </body>
+
+    </html>
     `
 }
 
